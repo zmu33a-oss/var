ca6f7b5aed6dddd712f65f29090751efbd3540ed
zmu33a-oss <zmu33a@gmail.com>
2026-04-01 00:12:22 +0300

    update file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
