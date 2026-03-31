d5b540faf714316785c168d25d7a2e4551cd2cc8
zmu33a-oss <zmu33a@gmail.com>
2026-03-31 21:48:33 +0300

    update file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,5 @@
-class NestedIterator:
-    def __init__(self, nestedList):
-        self.stack = [(nestedList, 0)]
-    
-    def next(self):
-        nestedList, i = self.stack[-1]
-        self.stack[-1] = (nestedList, i + 1)
-        return nestedList[i].getInteger()
-
-    def hasNext(self):
-        while self.stack:
-            nestedList, i = self.stack[-1]
-            if i == len(nestedList):
-                self.stack.pop()
-            else:
-                x = nestedList[i]
-                if x.isInteger():
-                    return True
-                self.stack[-1] = (nestedList, i + 1)
-                self.stack.append((x.getList(), 0))
-        return False
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
