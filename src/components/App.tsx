8ba5a1bee3bc87e906e18289ca2e6194b2e192ce
zmu33a-oss <zmu33a@gmail.com>
2026-04-02 03:02:41 +0300

    Update App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>هذا اختبار لسحب الكود بنجاح 🎯</h1>
+      <p>إذا شفت هذا النص في n8n يعني الغزال انصاد!</p>
+    </div>
+  );
+}
+
+export default App;
